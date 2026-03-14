1d43b58625cb4419d08f93eb11abd14353476ce5
dipak1920basnet <dipak1920basnet@gmail.com>
2026-03-14 15:06:13 +0545

    calcuate loan  and get final amount

---
+++ loan_interest.py
@@ -0,0 +1,9 @@
+loan_amount = 10000
+interest_rate = 12
+years = 5
+
+
+total_interest = (loan_amount * interest_rate * years) / 100
+final_payement = loan_amount + total_interest
+
+print(final_payement)
